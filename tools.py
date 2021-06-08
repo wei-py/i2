04bfd8a9c497afd574b907da843ea76f939951da
wei-py <78292849+wei-py@users.noreply.github.com>
2021-06-08 11:46:31 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-import requests, execjs, os, time
+import requests, execjs, os, time, pickle
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import ChromeOptions
@@ -72,8 +72,10 @@ class Hidden_Bro():
             return self.headless_bro()
 
 
-    def head_bro(self):
+    def head_bro(self, port=None):
         bro = webdriver.Chrome(executable_path='E:/chromedriver.exe', options=self.options)
+        if port:
+            self.options.add_argument(f'--proxy-server=http://127.0.0.1:{port}')  
         if os.path.exists('./identity.txt'):
             js = self.read_identity()
             bro.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
@@ -90,13 +92,13 @@ class Hidden_Bro():
 
 # 各大网站的登录 selenium版
 class login_web():
-    def __init__(self, view_bro=False):
+    def __init__(self, view_bro=False, port=None):
         if not view_bro:
-            self.bro = Hidden_Bro().head_bro()
+            self.bro = Hidden_Bro().head_bro(port)
         else:
-            self.bro = Hidden_Bro().head_bro()
+            self.bro = Hidden_Bro().headless_bro()
 
-    def login_tb(self, username, password, view_bro=False):
+    def login_tb(self, username, password):
         """
         username: 账号
         password: 密码
@@ -112,3 +114,35 @@ class login_web():
         self.bro.find_element_by_xpath('//*[@id="login-form"]/div[4]/button').click()
         time.sleep(3)
         return self.bro
+    
+    def login_tb_cookies(self, cookies):
+        if type(cookies) == str:
+            cookies = cookiesStrToDic(cookies)
+        self.bro.get("https://www.taobao.com")
+        for cookie in cookies:
+            self.bro.add_cookie({
+                "domain":".taobao.com",
+                "name":cookie,
+                "value":cookies[cookie],
+                "path":'/',
+                "expires":None
+            })
+        self.bro.get("https://www.taobao.com")
+        return self.bro
+
+if __name__ == '__main__':
+    # cookies = "thw=cn; hng=CN%7Czh-CN%7CCNY%7C156; cna=SNKXGFvrbxgCAXFE4HZczSFH; lgc=pgalimkt; tracknick=pgalimkt; mt=ci=7_1; sgcookie=E1006AiGcC6K3p69FtU%2BmESGo2fzBzrOhrK5BkPRGxWiuEkrXDdAnFH5SDOP9v01RxkxkcCPAHmFPvw2PSbOaGxaJg%3D%3D; _cc_=Vq8l%2BKCLiw%3D%3D; xlly_s=1; _m_h5_tk=5ef57936b24f9f9621726dd714506ee8_1623128399851; _m_h5_tk_enc=dd69db3fdb7058d9a77292d0230c0c6f; t=a2c9a1a583b2bb22b6022fe22524183b; _tb_token_=37ee90b33e1b; uc1=cookie14=Uoe2zshs80wyQw%3D%3D; isg=BHV1J6aVeYq83p0AmVTNgC3QhPEv8ikEioM1ZveaOuw7zpXAv0Cx1YdEGJJ4iUG8; l=eBNH4x-HjXEXgrWtBOfanurza77OSIRYYuPzaNbMiOCPOh5B592GW6_UcbY6C3GVh6-MR3l-cR2XBeYBqQAonxvtIosM_Ckmn; tfstk=cmuCBy_7g9XBn6xP8XONYc8z6QaRwX__sN2ZOmeBwXT3y-1DiZyb_6VCCNEY1"
+    # tb_login = login_web(port=9000).login_tb_cookies(cookies)
+    tb_login = login_web(port=9000).login_tb(username='pgalimkt', password='2021pgalimkt')
+    time.sleep(3)
+    # tb_login.execute_script('document.querySelector("#q").value="小米12"; document.querySelector("#J_TSearchForm > div.search-button > button").click()')
+    tb_login.find_element_by_xpath('//*[@id="q"]').send_keys('小米12')
+    time.sleep(3)
+    tb_login.find_element_by_xpath('//*[@id="J_TSearchForm"]/div[1]/button').click()
+    time.sleep(3)
+    tb_login.close()
+    time.sleep(3)
+    handles = tb_login.window_handles
+    tb_login.switch_to_window(handles[0])
+    with open('tb.html', 'w', encoding='utf-8') as f:
+        f.write(tb_login.page_source)
